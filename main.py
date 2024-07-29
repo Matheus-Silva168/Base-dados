3e84d92a328715982e8688d6edcbcdc610f0ada0
Matheus da Silva Gonçalves <omatheus3832@gmail.com>
2024-07-29 12:42:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def criar_conexao():
         print(f"Erro ao conectar ao MySQL: {e}")
     return conexao
 
+
 def criar_tabela(conexao):
     #Cria uma tabela na database do MySQL
     cursor = conexao.cursor()
@@ -48,17 +49,38 @@ def inserir_pagamento(conexao, nome,  valor, data_vencimento):
         print(f"Erro ao inserir o registro: {e}")
 
 
+def ler_pagamento(conexao):
+    # Lê a tabela
+    cursor = conexao.cursor()
+    query = f"SELECT * FROM pagamentos"
+    cursor.execute(query)
+    print(cursor.fetchall())
+
+
+def menu():
+    # Menu principal
+    while True:
+        print('\nOPÇÕES\n')
+        opcao=int(input('1 - Inserir pagamento.\n2 - Mostrar tabela.\n3 - Sair.\n'))
+        if opcao in range(1, 4):
+            return opcao
+            break
+
+
 def main():
     conexao = criar_conexao()
     if conexao is not None and conexao.is_connected():
         criar_tabela(conexao)
         while True:
-            nome = input("Digite o nome: ")
-            valor = float(input("Digite o valor: R$ ").strip())
-            data_vencimento = input("Digite a data de vencimento (YYYY-MM-DD): ").strip()
-            inserir_pagamento(conexao, nome, valor, data_vencimento)
-            continuar = input('Deseja inserir outro registro? (S/N)').upper().strip()
-            if continuar != 'S':
+            opcao = menu()
+            if opcao == 1:
+                nome = input("Digite o nome: ")
+                valor = float(input("Digite o valor: R$ ").strip())
+                data_vencimento = input("Digite a data de vencimento (YYYY-MM-DD): ").strip()
+                inserir_pagamento(conexao, nome, valor, data_vencimento)
+            if opcao == 2:
+                ler_pagamento(conexao)
+            if opcao == 3:
                 break
         conexao.close()
 
